ee5aab7cf3ed98df1a6040acd727233e5d53c7e2
Maelys Bassilekin Blanc <maelysbassilekinblanc@gmail.com>
2020-08-12 14:55:13 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create([{ name: 'Star Wars', address: '20 rue des dames', category: "french" }, { name: 'Boca Mexa', address: '20 rue truffaut', category: "chinese"}])
+
+Review.create({ content: "Good restaurant", rating: 4, restaurant_id: 1 })
